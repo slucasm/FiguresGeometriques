5a02c08f7d9dc313a4c139146c8899fb0f57ab77
unknown <sergi.lucas@estudiant.upc.edu>
2018-09-28 10:26:42 +0200

    Ya funcionan los test

--- src/test/java/com/company/RectangleTest.java
+++ src/test/java/com/company/RectangleTest.java
@@ -12,8 +12,8 @@ public class RectangleTest {
     {
         Rectangle r = new Rectangle(2,2);
         double area = r.getArea();
-        double esperado = 4;
-        assertEquals(esperado,area);
+        double esperado = 4.0;
+        assertEquals(esperado,area,0.0);
         //assertEquals(4,area);
     }
 }
